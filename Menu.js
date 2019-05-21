6cbd960f99afdf84046e602d119819542d15f6a2
Sdacm <salvador-3255@hotmail.com>
2019-05-21 03:10:42 +0300

    Buttons debug

--- Menu.js
+++ Menu.js
@@ -2,9 +2,9 @@ var Menu = {
 	
 	preload: function() {
     
-		game.load.image('lvl1', 'assets/lvl1.png', 200,200);
-		game.load.image('lvl2', 'assets/lvl2.png', 200,200);
-		game.load.image('background', 'assets/starfield.png', 800,600);
+		game.load.image('lvl1', 'assets/lvl1.png', 20,20);
+		game.load.image('lvl2', 'assets/lvl2.png', 20,20);
+		game.load.image('background', 'assets/starfield.png', 20,20);
 
 	},
 
@@ -19,8 +19,8 @@ var Menu = {
 
 		var bg = game.add.image(0,0, 'background');
 
-		button1 = game.add.button(100,250, "lvl1", click1 , this, function(){});
-		button2 = game.add.button(500,250, "lvl2", click2, this, function(){});
+		button1 = game.add.button(0,0, "lvl1", click1 , this, function(){});
+		button2 = game.add.button(0,0, "lvl2", click2, this, function(){});
     
 		function click1 () {
 			game.state.start('Level1');
