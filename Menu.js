c436eca2739244a8ea9ddd61902ec54f082c426f
Sdacm <salvador-3255@hotmail.com>
2019-05-21 03:15:41 +0300

    Menu debug

--- Menu.js
+++ Menu.js
@@ -2,8 +2,8 @@ var Menu = {
 	
 	preload: function() {
     
-		game.load.image('lvl1', 'assets/lvl1.png', 20,20);
-		game.load.image('lvl2', 'assets/lvl2.png', 20,20);
+		game.load.image('lvl1', 'assets/lvl1.png', 200,200);
+		game.load.image('lvl2', 'assets/lvl2.png', 200,200);
 		game.load.image('background', 'assets/starfield.png', 20,20);
 
 	},
@@ -17,10 +17,10 @@ var Menu = {
 		game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 		game.physics.startSystem(Phaser.Physics.ARCADE);
 
-		var bg = game.add.image(0,0, 'background');
+		var bg = game.add.image(-1300,20, 'background');
 
-		button1 = game.add.button(0,0, "lvl1", click1 , this, function(){});
-		button2 = game.add.button(0,0, "lvl2", click2, this, function(){});
+		button1 = game.add.button(100,1750, "lvl1", click1 , this, function(){});
+		button2 = game.add.button(1000,1750, "lvl2", click2, this, function(){});
     
 		function click1 () {
 			game.state.start('Level1');
